1788767831ab7aedac398711bc926f444cb2eaf9
machadinhacega <izav.machado@gmail.com>
2020-08-16 20:50:57 -0300

    Mudancas no GERENCIAR CONTAS

--- testes.py
+++ testes.py
@@ -63,8 +63,12 @@ Escolha sua opção
             else:
                 print('Digite uma opção válida')
 
-#Gerenciar Contas
     elif escolha1 == '2':
+        from funcoes_contas import adicionarConta
+        from funcoes_contas import consultarConta
+        from funcoes_contas import buscarContaStatus
+        from funcoes_contas import editarConta
+        from funcoes_contas import excluirConta
         print('##### GERENCIAR CONTAS #####')
         while True:
             opcao = input("""Escolha uma opção:
@@ -74,111 +78,31 @@ Escolha sua opção
 4 - Buscar conta por status
 5 - Editar
 6 - Excluir
-7 - Voltar
-►""")
-
-            # ## Adicionar
+0 - Voltar
+►  """)
             if opcao == '1':
-                nomeConta = input('Digite o nome da conta que deseja adicionar: ').title()
-                valor = input('Valor da conta de {}: '.format(nomeConta))
-                while valor.isnumeric() == False:
-                    valor = input('Digite um valor válido: ')
-                status = input("Qual a situação da conta de {}? (Pago ou Em aberto): ".format(nomeConta)).title()
-                while status != 'Pago' and status != 'Em Aberto':
-                    status = input("Por favor, digite uma das opções a seguir (Pago ou Em aberto): ").title()
-                contas[nomeConta] = {
-                    'Valor': float(valor),
-                    'Status': status,
-                    'Pagante': 'Ninguém pagou ainda :('
-                }
-                if contas[nomeConta]['Status'] == 'Pago':
-                    contas[nomeConta]['Pagante'] = input('Quem pagou a conta: ').title()
-
-                print('Conta de {} adicionada.'.format(nomeConta))
-
-            ## Consultar
+                adicionarConta(contas)
             if opcao == '2':
-                consultarConta = input('Digite a conta que deseja consultar: ').title()
-                while consultarConta not in contas:
-                    print(
-                        'A conta "{}" não esta cadastrada. Escolha uma das contas cadastradas:'.format(consultarConta))
-                    for k, v in contas.items():
-                        print(k, end='. ')
-                    consultarConta = input('\nDigite uma conta válida: ').title()
-                print('Dados da conta de {}'.format(consultarConta))
-                for k, v in contas[consultarConta].items():
-                    print(k, ': ', v)
-
-            # Ver todas as contas
+                consultarConta(contas)
             if opcao == '3':
-                for k, v in contas.items():
-                    print('\nDados da conta de {}: '.format(k))
-                    for k, v in contas[k].items():
-                        print(k, ': ', v)
-
-            # Buscar conta por status
+                if contas:
+                    for k, v in contas.items():
+                        print('\nDados da conta de {}: '.format(k))
+                        for k, v in contas[k].items():
+                            print(k, ': ', v)
+                    input('Digite ENTER para continuar')
+                else:
+                    print('Você não tem nenhuma conta cadastrada')
+                    input('Digite ENTER para continuar')
             if opcao == '4':
-                statusBusca = input('Você deseja buscar contas "Pagas" ou "Em aberto"? ').lower()
-                while statusBusca != 'pagas' and statusBusca != 'em aberto':
-                    statusBusca = input('Por favor, digite uma das opções a seguir (Pagas ou Em aberto): ').lower()
-                    for k, v in contas.items():
-                        print(k, end='. ')
-                if statusBusca == 'pagas':
-                    print('\nContas Pagas: ')
-                    for conta, v in contas.items():
-                        for k, v in contas[conta].items():
-                            if v == 'Pago':
-                                print(conta, end='. ')
-                elif statusBusca == 'em aberto':
-                    print('\nContas Em Aberto: ')
-                    for conta, v in contas.items():
-                        for k, v in contas[conta].items():
-                            if v == 'Em Aberto':
-                                print(conta, end='. ')
-
-            # Editar
+                buscarContaStatus(contas)
             if opcao == '5':
-                nomeConta = input('Digite o nome da conta que deseja Editar: ').title()
-                while nomeConta not in contas:
-                    print('A conta "{}" não esta cadastrada. Escolha uma das contas cadastradas:'.format(nomeConta))
-                    for k, v in contas.items():
-                        print(k, end='. ')
-                    nomeConta = input('\nDigite uma conta válida: ').title()
-
-                print('Dados anteriores da conta de {}'.format(nomeConta))
-                for k, v in contas[nomeConta].items():
-                    print(k, ': ', v, end=' | ')
-                print('\n')
-
-                valor = input('Novo valor da conta de {}: '.format(nomeConta))
-                while valor.isnumeric() == False:
-                    valor = input('Digite um valor válido: ')
-                status = input("Nova situação da conta de {}? (Pago ou Em aberto): ".format(nomeConta)).title()
-                while status != 'Pago' and status != 'Em Aberto':
-                    status = input("Por favor, digite uma das opções a seguir (Pago ou Em aberto): ").title()
-                contas[nomeConta] = {
-                    'Valor': float(valor),
-                    'Status': status,
-                    'Pagante': 'Ninguém pagou ainda :('
-                }
-                if contas[nomeConta]['Status'] == 'Pago':
-                    contas[nomeConta]['Pagante'] = input('Quem pagou a conta: ').title()
-                print('\nConta de {} editada com sucesso.'.format(nomeConta))
-
-            # Excluir
+                editarConta(contas)
             if opcao == '6':
-                nomeConta = input('Digite o nome da conta que deseja Editar: ').title()
-                while nomeConta not in contas:
-                    print('A conta "{}" não esta cadastrada. Escolha uma das contas cadastradas:'.format(nomeConta))
-                    for k, v in contas.items():
-                        print(k, end='. ')
-                    nomeConta = input('\nDigite uma conta válida: ').title()
-                contas.pop(nomeConta)
-                print(contas)
-
-            # Voltar
-            if opcao == '7':
+                excluirConta(contas)
+            if opcao == '0':
                 break
+
 
     elif escolha1 == '3':
         print('##### MOSTRAR VALORES #####')
@@ -189,5 +113,3 @@ Escolha sua opção
         break
     else:
         print('Digite uma opção válida')
-
-
