1db1f49cf13aeaf70cf91dfbdd4cf41e69763b1a
machadinhacega <izav.machado@gmail.com>
2020-08-20 19:11:05 -0300

    Add loop nas funções

--- funcoes.py
+++ funcoes.py
@@ -1,93 +1,115 @@
+# --------------------------------------------------------------------------
+# ----------------------  GERENCIAR PERFIL  --------------------------------
+# --------------------------------------------------------------------------
+
 def adicionarUsuario(todos):
-  nomeusuario = input('Digite seu nome: ').title().strip()
-  todos.append(nomeusuario)
-  print('Seja bem vindo ao Roomath,', nomeusuario)
   while True:
+    if len(todos) < 2:
+      nomeusuario = input('Digite um nome para adicionar: ').title().strip()
+      todos.append(nomeusuario)
+      print('Cadastro de {} realizado com sucesso'.format(nomeusuario))
       questNovoUsuario = input('Você gostaria de adicionar mais um perfil? ').lower()
-      if questNovoUsuario == 'sim':
-          nomeusuario = input('Digite um novo nome: ').title().strip()
-          todos.append(nomeusuario)
-          print('Seja bem vindo ao Roomath,', nomeusuario)
-      elif questNovoUsuario == 'não':
-          break
-      else:
-          print('Por favor, digite SIM ou NÃO.')
+      if questNovoUsuario == 'não':
+        break
+      elif questNovoUsuario != 'sim' and questNovoUsuario != 'não':
+        print('Por favor, digite SIM ou NÃO.')
+    else:
+      print('Você já tem 2 usuários cadastrados e nosso programa é limitadíssimo')
+      input('Digite ENTER para continuar')
+      break
+
+
 
 
 def editarUsuario(todos):
-  if todos:
-    editarUsuario = input('Digite o nome do usuário a ser editado: ').title()
-    while True:
-      if editarUsuario not in todos:
-        print('Usuário não cadastrado, escolha um usuário válido:')
-        for nome in todos:
-          print(nome)
-        editarUsuario = input('Informe um usuário cadastrado: ').title()
-      else:
-        todos.remove(editarUsuario)
-        correcaoUsuario = input('Informe um novo nome para {}: '.format(editarUsuario)).title()
-        todos.append(correcaoUsuario)
-        print(todos)
+# Essa condição verifica se a lista "todos" tem algum elemento nela ou não
+  while True:
+    if todos:
+      editarUsuario = input('Digite o nome do usuário a ser editado: ').title()
+      while editarUsuario not in todos:
+          print('Usuário não cadastrado, escolha um usuário válido:')
+          for nome in todos:
+            print(nome)
+          editarUsuario = input('Digite o nome do usuário a ser editado: ').title()
+      correcaoUsuario = input('Informe um novo nome para {}: '.format(editarUsuario)).title()
+      todos.insert(todos.index(editarUsuario), correcaoUsuario)
+      todos.remove(editarUsuario)
+      print('Usuário atualizado com sucesso.')
+      questEditarUsuario = input('Deseja editar novamente? ').lower()
+      while questEditarUsuario != 'não' and questEditarUsuario != 'sim':
+        questEditarUsuario = input('Por favor, digite SIM ou NÃO \nDeseja editar novamente? ').lower()
+      if questEditarUsuario == 'não':
         break
-        while True:
-          questEditarUsuario = input('Deseja editar novamente? ').lower()
-          if questEditarUsuario == 'sim':
-            editarUsuario(todos)
-          elif questEditarUsuario == 'não':
-            break
-          else:
-            print('Por favor, digite SIM ou NÃO')
-  else:
-    print('Você não tem nenhum usuário cadastrado')
-    input('Digite ENTER para continuar')
+    else:
+      print('Você não tem nenhum usuário cadastrado')
+      input('Digite ENTER para continuar')
 
 
 
 def excluirUsuario(todos):
-  if todos:
-    print('Lista de perfis:\n', todos)
-    while True:
-      exUsuario = input('Digite o nome do usuário a ser excluído: ').title()
-      if exUsuario not in todos:
-        print ('O usuário não foi cadastrado.')
-      else:
-        todos.remove(exUsuario)
-        print ('Lista de usuários atualizada: {}'.format(todos))
-        break
-  else:
-    print('Você não tem nenhum usuário cadastrado')
-    input('Digite ENTER para continuar')
-
-
-
-########################
-### GERENCIAR CONTAS ###
-########################
-
-def adicionarConta(contas):
-  nomeConta = input('Digite o nome da conta que deseja adicionar: ').title()
-  valor = input('Valor da conta de {}: '.format(nomeConta))
-  # Verificando se o valor digitado pode ser FLOAT
   while True:
-    try:
-      float(valor)
+    if todos:
+      print('Lista de perfis:\n', todos)
+      exUsuario = input('Digite o nome do usuário a ser excluído: ').title()
+      while exUsuario not in todos:
+        exUsuario = input('O usuário não foi cadastrado. \nDigite o nome do usuário a ser excluído: ').title()
+      todos.remove(exUsuario)
+      print ('Lista de usuários atualizada')
+      questexUsuario = input('Você deseja excluir outro usuário? ').lower()
+      while questexUsuario != 'não' and questexUsuario != 'sim':
+        questexUsuario = input('Por favor, digite SIM ou NÃO. \nVocê deseja excluir outro usuário? ').lower()
+      if questexUsuario == 'não':
+        break
+    else:
+      print('Você não tem nenhum usuário cadastrado')
+      input('Digite ENTER para continuar')
       break
-    except:
-      valor = input('Digite um valor válido: ')
-  status = input("Qual a situação da conta de {}? (Pago ou Em aberto): ".format(nomeConta)).title()
-  # restringindo o status para receber apenas Pago ou Em aberto
-  while status != 'Pago' and status != 'Em Aberto':
-    status = input("Por favor, digite uma das opções a seguir (Pago ou Em aberto): ").title()
-  contas[nomeConta] = {
-    'Valor': float(valor),
-    'Status': status,
-    'Pagante': 'Ninguém pagou ainda :('
-  }
-  if contas[nomeConta]['Status'] == 'Pago':
-    contas[nomeConta]['Pagante'] = input('Quem pagou a conta: ').title()
 
-  print('Conta de {} adicionada.'.format(nomeConta))
-  input('Digite ENTER para continuar')
+
+# --------------------------------------------------------------------------
+# ---------------------  GERENCIAR CONTAS  ---------------------------------
+# --------------------------------------------------------------------------
+
+def adicionarConta(contas,todos):
+  while True:
+    nomeConta = input('Digite o nome da conta que deseja adicionar: ').title()
+    valor = input('Valor da conta de {}: '.format(nomeConta))
+    # Verificando se o valor digitado pode ser FLOAT
+    while True:
+      try:
+        float(valor)
+        break
+      except:
+        valor = input('Digite um valor válido: ')
+    status = input("Qual a situação da conta de {}? (Pago ou Em aberto): ".format(nomeConta)).title()
+    # restringindo o status para receber apenas Pago ou Em aberto
+    while status != 'Pago' and status != 'Em Aberto':
+      status = input("Por favor, digite uma das opções a seguir (Pago ou Em aberto): ").title()
+    contas[nomeConta] = {
+      'Valor': float(valor),
+      'Status': status,
+      'Pagante': 'Ninguém pagou ainda :('
+    }
+    if contas[nomeConta]['Status'] == 'Pago':
+      if todos:
+        print('Quem pagou a conta? Escolha um dos perfis registrados:')
+        for i in todos:
+          print(i)
+        contas[nomeConta]['Pagante'] = input('► ').title().strip()
+        while contas[nomeConta]['Pagante'] not in todos:
+          print('Por favor, escolha um perfil registrado')
+          for i in todos:
+            print(i)
+          contas[nomeConta]['Pagante'] = input('Quem pagou a conta: ').title().strip()
+      else:
+        print('Você não pode adicionar pagantes por que não tem nenhum perfil cadastrado.'
+              '\nMas fique tranquile, você pode editar essa conta futuramente.')
+    print('Conta de {} adicionada.'.format(nomeConta))
+    questAdicionarConta = input('Você deseja adicionar outra conta?  ').lower()
+    while questAdicionarConta != 'não' and questAdicionarConta != 'sim':
+      questAdicionarConta = input('Por favor, digite SIM ou NÃO. \nVocê deseja excluir outro usuário? ').lower()
+    if questAdicionarConta == 'não':
+      break
 
 
 def consultarConta(contas):
